8422563f9a620f37574d317e0bd948afb46b45dd
Richard Partridge <richard.partridge@headforwards.com>
2017-01-18 11:27:30 +0000

    First Change

---
+++ main.py
@@ -0,0 +1,7 @@
+#Sample code for a HTML widget
+
+def get(event, context):
+    return {
+        'type': 'html',
+        'html': '<img src="https://joemainzone-ilyznmosqlw0zs.netdna-ssl.com/wp-content/uploads/2011/07/ASCII.png" width="400px">'
+    };
